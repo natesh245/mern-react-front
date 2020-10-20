f4195aa5cf4b7c76086fdc3d6400f0c5aa3d7d18
Natesh <natesh246@gmail.com>
2020-10-20 11:50:15 +0530

    added redux to get and delete todo

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -5,26 +5,30 @@ import Todo from "./Todo/Todo";
 
 import axios from "axios";
 
+import { store } from "./../index";
+import { connect } from "react-redux";
+
 class TodoList extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      todoList: [],
-      deleteId: 0,
-    };
+    // this.state = {
+    //   todoList: [],
+    //   deleteId: 0,
+    // };
     this.deleteHandler = this.deleteHandler.bind(this);
   }
 
   deleteHandler(todoId) {
     console.log(todoId);
-    this.setState({ deleteId: todoId });
+    // this.setState({ deleteId: todoId });
+    store.dispatch({ type: "DELETE_TODO", deleteId: todoId });
   }
 
   render() {
     return (
       <div className="todo-list">
-        {this.state.todoList.reverse().map((todo, index) => {
+        {this.props.todoList.reverse().map((todo, index) => {
           return (
             <Todo
               title={todo.title}
@@ -43,23 +47,29 @@ class TodoList extends React.Component {
   componentDidMount() {
     axios.get("http://localhost:4000/todos/").then((response) => {
       console.log(response.data);
-      this.setState({ todoList: response.data });
+      store.dispatch({ type: "GET_ALL_TODO", data: response.data });
+      // this.setState({ todoList: response.data });
     });
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.deleteId !== this.state.deleteId) {
+    if (prevProps.deleteId !== this.props.deleteId) {
       axios
-        .delete(`http://localhost:4000/todos/${this.state.deleteId}`)
+        .delete(`http://localhost:4000/todos/${this.props.deleteId}`)
         .then((response) => console.log(response))
         .then(() => {
           axios.get("http://localhost:4000/todos/").then((response) => {
             console.log(response.data);
-            this.setState({ todoList: response.data });
+            store.dispatch({ type: "GET_ALL_TODO", data: response.data });
           });
         });
     }
   }
 }
 
-export default TodoList;
+const mapStateToProps = (state) => ({
+  todoList: state.todoList,
+  deleteId: state.deleteId,
+});
+
+export default connect(mapStateToProps)(TodoList);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const initialState = {
+  todoList: [],
+  deleteId: 0,
+
+  titleInput: "",
+  teaxtArea: "",
+  message: "",
+  showMessage: false,
+};
+
+function todoReducer(state = initialState, action) {
+  switch (action.type) {
+    case "GET_ALL_TODO":
+      return {
+        ...state,
+
+        todoList: action.data,
+      };
+    case "DELETE_TODO":
+      return {
+        ...state,
+        deleteId: action.deleteId,
+      };
+
+    default:
+      return state;
+  }
+}
+
+export const store = createStore(todoReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
